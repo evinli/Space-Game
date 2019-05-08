9bf88b01ab52453c5810a2e725c2e0aaa86ed0c1
mason-anderson <masonanderson0@gmail.com>
2019-05-08 12:51:23 -0700

    added sprite class and empty functions for Board

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,15 @@
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Board extends JPanel {
+public class Board extends JPanel implements ActionListener {
 
     public Board() {}
+
+        private void draw() {};
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
---
+++ src/Sprite.java
@@ -0,0 +1,15 @@
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+public class Sprite {
+    private Image image;
+    private int x, y;
+
+    public Sprite(String imageFile) {}
+
+    private void loadImage() {}
+    public Image getImage() { return null; }
+
+    public void move() {}
+}
