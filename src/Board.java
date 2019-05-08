93758ab9334d5275a33a691da8c9b5e49da44f1a
mason-anderson <masonanderson0@gmail.com>
2019-05-08 13:15:48 -0700

    spaceship is now drawn to the screen

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,41 @@
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.List;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 
 public class Board extends JPanel implements ActionListener {
+    private Sprite spaceShip;
 
-    public Board() {}
+
+    public Board() {
+        spaceShip = new Sprite("res/thing_test.png");
+        spaceShip.setX(10);
+    }
 
         private void draw() {};
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.drawImage(spaceShip.getImage(), spaceShip.getX(),
+                spaceShip.getY(), this);
+
+        Toolkit.getDefaultToolkit().sync();
     }
 }
--- src/Sprite.java
+++ src/Sprite.java
@@ -5,11 +5,40 @@ import javax.swing.ImageIcon;
 public class Sprite {
     private Image image;
     private int x, y;
+    private int dx, dy;
 
-    public Sprite(String imageFile) {}
+    public Sprite(String imageFile) {
+        loadImage(imageFile);
+    }
 
-    private void loadImage() {}
-    public Image getImage() { return null; }
+    private void loadImage(String imageName) {
+
+        ImageIcon i = new ImageIcon(imageName);
+        image = i.getImage();
+    }
 
     public void move() {}
+
+    private void loadImage() {}
+
+    public Image getImage() {
+        return image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
